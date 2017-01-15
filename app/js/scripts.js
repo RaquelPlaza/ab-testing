a52fb2d762b07fcd02aabbde95cb8259327695fe
Raquel Plaza <contact@raquelplaza.com>
2017-01-15 16:47:44 +0100

    styles and scripts files added

---
+++ app/js/scripts.js
@@ -0,0 +1,79 @@
+// javascript 
+document.addEventListener('DOMContentLoaded', function() {
+/**
+ * @param  {[type]}
+ * @return {[type]}
+ */
+ function getData(variant) {
+ 	var xhttp = new XMLHttpRequest();
+ 	xhttp.onreadystatechange = function() {
+ 		if (this.readyState == 4 && this.status == 200) {
+ 			var allPrograms = JSON.parse(this.responseText);
+ 			replaceElements(variant, allPrograms);
+ 		}
+ 	};
+ 	xhttp.open("GET", "data/data.json", true);
+ 	xhttp.send();
+
+/**
+ * @param  {[type]}
+ * @param  {[type]}
+ * @return {[type]}
+ */
+ function replaceElements(variant, data) {
+ 	var variant = variant;
+ 	var title = "",
+ 	episode_title = "",
+ 	category = "",
+ 	image = "";
+
+ 	for(var i = 0; i < data.length; i++) {
+
+ 		if (data[i].id == variant) {
+
+ 			title = data[i].brand_title;
+ 			episode_title = data[i].episode_title;
+ 			category = data[i].category;
+ 			image_path = data[i].image;
+
+ 			var titleContainer = document.getElementsByClassName("thumbnail-item__title"),
+ 			episodeContainer = document.getElementsByClassName("thumbnail-item__subtitle"),
+ 			catContainer = document.getElementsByClassName("thumbnail-item__desc__label"),
+ 			imageContainer = document.getElementsByClassName("rs-image"),
+ 			imageTagSourceSource = document.getElementsByTagName("source")[0],
+ 			imageTagImgSource = document.getElementsByTagName("img")[0];
+
+ 			titleContainer[0].innerHTML = title;
+ 			episodeContainer[0].innerHTML = episode_title;
+ 			catContainer[0].innerHTML = category;
+ 			imageTagSourceSource.setAttribute("srcset", image_path);
+ 			imageTagImgSource.setAttribute("srcset", image_path);
+ 		}
+
+
+ 	}
+ 	
+
+ }
+
+}
+
+
+/**
+ * @param  {[type]}
+ * @return {[type]}
+ */
+ function getParameterByName(name) {
+ 	var url = window.location.href;
+ 	var name = name.replace(/[\[\]]/g, "\\$&");
+ 	var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+ 	results = regex.exec(url);
+ 	if (!results) return null;
+ 	if (!results[2]) return '';
+ 	return decodeURIComponent(results[2].replace(/\+/g, " "));
+ }
+
+ var variant = getParameterByName('variant');
+ getData(variant);
+
+});
